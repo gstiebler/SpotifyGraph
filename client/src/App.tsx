43cea4155a2f8028b956dea120c0d7135573a521
Gui Stiebler <gms.public@stiebler.me>
2024-10-15 15:15:56 -0700

    showing saved tracks :)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,83 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AccessToken, SpotifyApi } from '@spotify/web-api-ts-sdk';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+const clientId = "88ea8220c6e443d9aec4aee0405c51eb";
+const redirectUri = "http://localhost:3000/callback";
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Login() {
+  // print the access token
+  console.log(window.location.href);
+  return <h2>Login</h2>;
+}
+
+let spotifyToken: AccessToken;
 
 function App() {
+
+  const handleButtonClick = async () => {
+    SpotifyApi.performUserAuthorization(clientId, redirectUri, ["user-library-read"], async (token: AccessToken) => {
+      spotifyToken = token;
+      console.log(token);
+    });
+  };
+
+  const showSomethingClick = async () => {
+    const api = SpotifyApi.withAccessToken(clientId, spotifyToken);
+    const tracks = await api.currentUser.tracks.savedTracks();
+    console.table(tracks.items.map((item) => ({
+      name: item.track.name,
+      artists: item.track.artists.map((artist) => artist.name).join(", "),
+    })));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/callback" element={<Login />} />
+          </Routes>
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={handleButtonClick}>Get User Profile</button>
+          <button onClick={showSomethingClick}>Show something</button>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
