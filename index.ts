1559c95aa019febbb54d1087bfdcee38bf096080
Gui Stiebler <gms.public@stiebler.me>
2024-10-14 21:28:16 -0700

    getting beatles songs

--- index.ts
+++ index.ts
@@ -1,41 +1,22 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
-import { SpotifyWebApi } from '@spotify/web-api-ts-sdk';
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
 dotenv.config();
 
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
+const client_id = process.env.CLIENT_ID!;
+const client_secret = process.env.CLIENT_SECRET!;
 
-const authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  method: 'post',
-  headers: {
-    'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64'),
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  data: new URLSearchParams({
-    grant_type: 'client_credentials'
-  })
-};
-
-async function getAccessToken(): Promise<string> {
-  try {
-    const response = await axios(authOptions);
-    if (response.status === 200) {
-      const token = response.data.access_token;
-      return token;
-    }
-  } catch (error: any) {
-    console.error('Error:', error.message);
-  }
-
-  return '';
-}
+const api = SpotifyApi.withClientCredentials(client_id, client_secret);
 
 async function main() {
-  const token = await getAccessToken();
-  console.log('Token:', token);
+  const items = await api.search("The Beatles", ["artist"]);
+
+  console.table(items.artists.items.map((item) => ({
+    name: item.name,
+    followers: item.followers.total,
+    popularity: item.popularity,
+  })));
 }
 
 main();
