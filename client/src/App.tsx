f3a912c6db699151c30b86dc739dc7027f7eff95
Gui Stiebler <gms.public@stiebler.me>
2024-10-15 22:09:35 -0700

    caching artists

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,26 +24,44 @@ function Login() {
 
 const App: React.FC = () => {
 
+  const [artistsMap, setArtistsMap] = useState<Map<string, SimplifiedArtist>>(new Map());
+  const [artistsRelationshipPairs, setArtistsRelationshipPairs] = useState<string[][]>([]);
+
   useEffect(() => {
 
     const getArtists = async (token: AccessToken) => {
       const api = SpotifyApi.withAccessToken(clientId, token!);
       const tracks = await api.currentUser.tracks.savedTracks(50);
-      let artistsMap = new Map<string, SimplifiedArtist>();
+      let artistsMapLocal = new Map<string, SimplifiedArtist>();
       console.table(tracks.items.map((item) => ({
         name: item.track.name,
         artists: item.track.artists.map((artist) => artist.name).join(", "),
       })));
   
-      tracks.items.forEach((item) => {
-        item.track.artists.forEach((artist) => {
-          artistsMap.set(artist.id, artist);
-        });
-      });
+      for (const track of tracks.items) {
+        for (const artist of track.track.artists) {
+          artistsMapLocal.set(artist.id, artist);
+        }
+      }
+
+      const artistsRelationships = new Set<string>();
   
-      const artistsIds = Array.from(artistsMap.keys());
+      const artistsIds = Array.from(artistsMapLocal.keys());
       const getRelatedArtistsPromise = await artistsIds.map(async (id) => [id, (await api.artists.relatedArtists(id)).artists]);
       const relatedArtistsListMap = (await Promise.all(getRelatedArtistsPromise));
+      for (const [id, relatedArtists] of relatedArtistsListMap) {
+        for (const relatedArtist of relatedArtists) {
+          artistsMapLocal.set((relatedArtist as Artist).id, relatedArtist as Artist);
+          const artistKey = `${id}-${(relatedArtist as Artist).id}`;
+          artistsRelationships.add(artistKey);
+        }
+      }
+      const artistsRelationshipPairsLocal = Array.from(artistsRelationships).map((key) => key.split("-"));
+      localStorage.setItem('artistsRelationshipPairs', JSON.stringify(artistsRelationshipPairsLocal));
+      localStorage.setItem('artistsMap', JSON.stringify(Array.from(artistsMapLocal.entries())));
+      setArtistsMap(artistsMapLocal);
+      setArtistsRelationshipPairs(artistsRelationshipPairsLocal);
+
       console.log(relatedArtistsListMap);
     };
 
@@ -52,7 +70,14 @@ const App: React.FC = () => {
         return;
       }
       console.log(spotifyToken);
-      // getArtists(spotifyToken);
+      if (localStorage.getItem('artistsMap')) {
+        const artistsMapLocal = new Map<string, SimplifiedArtist>(JSON.parse(localStorage.getItem('artistsMap')!));
+        const artistsRelationshipPairsLocal = JSON.parse(localStorage.getItem('artistsRelationshipPairs')!);
+        setArtistsMap(artistsMapLocal);
+        setArtistsRelationshipPairs(artistsRelationshipPairsLocal);
+      } else {
+        getArtists(spotifyToken);
+      }
     });
   }, []);
 
@@ -78,7 +103,7 @@ const App: React.FC = () => {
             <Route path="/callback" element={<Login />} />
           </Routes>
         </header>
-        <Graph />
+        <Graph artistsMap={artistsMap} artistsRelationshipPairs={artistsRelationshipPairs}/>
       </div>
     </BrowserRouter>
   );
--- client/src/Graph.tsx
+++ client/src/Graph.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
+import { SimplifiedArtist } from '@spotify/web-api-ts-sdk';
 
 const nodes = [
-    { name: 'A' },
-    { name: 'B' },
-    { name: 'C' },
-    { name: 'D' },
-    { name: 'E' },
-    { name: 'F' },
-    { name: 'G' },
-    { name: 'H' },
-  ] as any;
-  
-  const links = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+] as any;
+
+const links = [
     { source: nodes[0], target: nodes[1] },
     { source: nodes[0], target: nodes[2] },
     { source: nodes[0], target: nodes[3] },
@@ -21,74 +22,73 @@ const nodes = [
     { source: nodes[3], target: nodes[7] },
     { source: nodes[4], target: nodes[5] },
     { source: nodes[4], target: nodes[7] }
-  ];
-  
-  function executeD3(svg: any) {
+];
+
+function executeD3(svg: any) {
     const width = 400, height = 300;
     const simulation = d3.forceSimulation(nodes)
-      .force('charge', d3.forceManyBody().strength(-100))
-      .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('link', d3.forceLink(links).id((d: any) => d.index))
-      .on('tick', () => ticked(svg));
-  }
-  
-  function updateLinks(svg: any) {
+        .force('charge', d3.forceManyBody().strength(-100))
+        .force('center', d3.forceCenter(width / 2, height / 2))
+        .force('link', d3.forceLink(links).id((d: any) => d.index))
+        .on('tick', () => ticked(svg));
+}
+
+function updateLinks(svg: any) {
     const u = svg
-      .selectAll('line')
-      .data(links)
-      .join('line')
-      .style('stroke', 'black')
-      .attr('x1', function (d: any) {
-        return d.source.x;
-      })
-      .attr('y1', function (d: any) {
-        return d.source.y;
-      })
-      .attr('x2', function (d: any) {
-        return d.target.x;
-      })
-      .attr('y2', function (d: any) {
-        return d.target.y;
-      });
-  }
-  
-  function updateNodes(svg: any) {
+        .selectAll('line')
+        .data(links)
+        .join('line')
+        .style('stroke', 'black')
+        .attr('x1', function (d: any) {
+            return d.source.x;
+        })
+        .attr('y1', function (d: any) {
+            return d.source.y;
+        })
+        .attr('x2', function (d: any) {
+            return d.target.x;
+        })
+        .attr('y2', function (d: any) {
+            return d.target.y;
+        });
+}
+
+function updateNodes(svg: any) {
     const u = svg
-      .selectAll('text')
-      .data(nodes)
-      .join('text')
-      .text(function (d: any) {
-        return d.name;
-      })
-      .attr('x', function (d: any) {
-        return d.x;
-      })
-      .attr('y', function (d: any) {
-        return d.y;
-      })
-      .attr('dy', function (d: any) {
-        return 5;
-      });
-  }
-  
-  function ticked(svg: any) {
+        .selectAll('circle')
+        .data(nodes)
+        .join('circle')
+        .attr('r', 5)
+        .attr('cx', function (d: any) {
+            return d.x;
+        })
+        .attr('cy', function (d: any) {
+            return d.y;
+        })
+        .style('fill', 'blue');
+}
+
+function ticked(svg: any) {
     updateNodes(svg);
     updateLinks(svg);
-  }
-  
+}
 
-export const Graph: React.FC = () => {
+
+export const Graph: React.FC<{
+    artistsMap: Map<string, SimplifiedArtist>,
+    artistsRelationshipPairs: string[][],
+}> = ({ artistsMap, artistsRelationshipPairs}) => {
 
 
     const svgRef = useRef<SVGSVGElement | null>(null);
 
     useEffect(() => {
-      if (svgRef.current) {
-        const svg = d3.select(svgRef.current);
-        executeD3(svg);
-      }
+        if (svgRef.current) {
+            const svg = d3.select(svgRef.current);
+            executeD3(svg);
+        }
     }, []);
-    
+
     return (
         <svg ref={svgRef} width={500} height={300} />
     );
